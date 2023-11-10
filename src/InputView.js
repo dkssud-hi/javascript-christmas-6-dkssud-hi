0d46e521b00560ee8804e5c4dbec5d8170e7148f
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-10 23:44:55 +0900

    feat: 예상 방문 날짜 입력 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -3,14 +3,19 @@ import { ERROR_MESSAGE } from './constants/ErrorMessage';
 
 const InputView = {
   async readDate() {
-    const date = Number(
-      await Console.readLineAsync(
-        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
-      )
-    );
-    this.validateDate(date);
+    try {
+      const date = Number(
+        await Console.readLineAsync(
+          '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
+        )
+      );
+      this.validateDate(date);
 
-    return date;
+      return date;
+    } catch (err) {
+      Console.print(err);
+      return this.readDate();
+    }
   },
 
   validateDate(date) {
@@ -23,6 +28,7 @@ const InputView = {
     const order = await Console.readLineAsync(
       '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)'
     );
+    //.split(',').map(el=>el.trim())
 
     return order;
   },
