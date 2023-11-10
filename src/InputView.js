2536dccd77e8a209b925395a4cf7d5a7e58b7784
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-10 15:49:50 +0900

    feat: 입력 기능 작성
    InputView에
    readDate : 예상 방문 날짜 입력받기
    readOrder : 주문할 메뉴와 개수를 입력받기
    두개의 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -2,12 +2,20 @@ import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(
+    const date = await Console.readLineAsync(
       '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)'
     );
-    // ...
+
+    return date;
   },
-  // ...
+
+  async readOrder() {
+    const order = await Console.readLineAsync(
+      '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)'
+    );
+
+    return order;
+  },
 };
 
 export default InputView;
