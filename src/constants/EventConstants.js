534d2eebc54ae582896b89fefafc927826e63f9c
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-11 21:40:00 +0900

    feat: 특별 이벤트 날짜를 나타내느 상수객체 추가

--- src/constants/EventConstants.js
+++ src/constants/EventConstants.js
@@ -21,3 +21,7 @@ export const EVENT = {
 export const WEEKDAY_EVENT = {
   NON_APPLICABLE_DATES: [1, 2, 8, 9, 15, 16, 22, 23, 29, 30],
 };
+
+export const SPECIAL_EVENT = {
+  APPLICABLE_DATES: [3, 10, 17, 24, 25, 31],
+};
