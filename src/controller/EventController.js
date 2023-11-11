dc1cbfb2b48679ead25726e8042101376f033f65
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-11 13:08:28 +0900

    feat: 컨트롤러에 할인 전 총 주문 금액을 계산하는 메소드 작성
    EventController 클래스에 할인 전 총 주문 금액을 계산하는 메소드 추가

--- src/controller/EventController.js
+++ src/controller/EventController.js
@@ -1,3 +1,14 @@
-class EventController {}
+import { PRICE_OF_MENUS } from '../constants/MenuInfo';
+
+class EventController {
+  calculateTotalAmountBeforeDiscount(menus) {
+    let amount = 0;
+    menus.forEach((menu) => {
+      amount += Number(PRICE_OF_MENUS[menu.name]) * Number(menu.quantity);
+    });
+
+    return amount;
+  }
+}
 
 export default EventController;
