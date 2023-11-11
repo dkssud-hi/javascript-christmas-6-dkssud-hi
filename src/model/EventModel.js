c4ae089dcbd3aadacab8788a98a0e72ed95a92db
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-11 12:24:17 +0900

    style: EventModel 클래스 주석 추가
    summarizeOrder 메소드의 반환값의 형식 설명 추가

--- src/model/EventModel.js
+++ src/model/EventModel.js
@@ -23,7 +23,10 @@ class EventModel {
       .split(',')
       .map((el) => el.trim())
       .map((el) => el.split('-'));
-
+    /* 
+     주문 정렬 형식
+     : [['메뉴1','메뉴1의 수량'],['메뉴2','메뉴2의 수량'],['메뉴3','메뉴3의 수량']]
+     */
     return summary;
   }
 }
