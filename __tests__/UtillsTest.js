b080cffa416583f07e56b6dc4573620f30eae930
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-12 18:08:46 +0900

    fix: summarizeOrder 테스트코드 오류 수정
    이전의 경로에서 호출되어 발생된 오류 제거

--- __tests__/UtillsTest.js
+++ __tests__/UtillsTest.js
@@ -14,10 +14,7 @@ describe('유틸함수 테스트', () => {
   });
 
   test('데이터 활용에 용이하게 format을 바꾸어주는 유틸함수 테스트 (summarizeOrder)', () => {
-    const mockModel = new EventModel();
-    const result = mockModel.summarizeOrder(
-      '해산물파스타-2,레드와인-1,초코케이크-1'
-    );
+    const result = summarizeOrder('해산물파스타-2,레드와인-1,초코케이크-1');
 
     expect(result).toEqual([
       { name: '해산물파스타', quantity: '2' },
