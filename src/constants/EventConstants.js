f94a41d29c60f5693ce94bd8698fd1a6b6957472
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-13 15:48:36 +0900

    feat: 혜택 내용을 담은 상수객체 생성

--- src/constants/EventConstants.js
+++ src/constants/EventConstants.js
@@ -26,3 +26,11 @@ export const WEEKDAY_EVENT = {
 export const SPECIAL_EVENT = {
   APPLICABLE_DATES: [3, 10, 17, 24, 25, 31],
 };
+
+export const BENEFIT_LIST = [
+  ['크리스마스 디데이 할인'],
+  ['평일 할인'],
+  ['주말 할인'],
+  ['특별 할인'],
+  ['증정 이벤트'],
+];
