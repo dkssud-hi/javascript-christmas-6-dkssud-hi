36fdaaf47f68854cdc3ce4be14b7d999d875486d
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-14 14:26:44 +0900

    style: 파일명에 공백 제거

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -6,6 +6,7 @@ export const ERROR_MESSAGE = Object.freeze({
   NOT_VALID_ORDER_QUANTITY: `${PREFIX} 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
   NOT_VALID_MENU_FORMAT: `${PREFIX} 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
   MENU_DUPLICATION: `${PREFIX} 유효하지 않은 주문입니다. 다시 입력해 주세요.`,
+  IS_ONLY_DRINK: `${PREFIX} 음료만 주문하실 수 없습니다. 주의사항을 확인하신 후 다시 입력해 주세요.`,
 });
 
 export const MESSAGE = Object.freeze({
