2e79dd93301b173238d29b5aa9d62343affa5f38
sang-il Yoon <yunsangil7749@gmail.com>
2023-11-15 12:16:42 +0900

    refactor: 컨트롤러 유틸함수 컨트롤러 내장 메소드로 변경
    해당 메소드들이 유틸함수로서 부적절 하고 컨트롤러 클래스의 내장 메소드로서 역할을 한다고 재 판단되어 복구진행

--- src/controller/EventController.js
+++ src/controller/EventController.js
@@ -1,3 +1,13 @@
+import {
+  BENEFIT_LIST,
+  CHRISMAS_D_DAY,
+  EVENT,
+  GIVEAWAY_EVENT,
+  SPECIAL_EVENT,
+  STATUS,
+  WEEKDAY_EVENT,
+} from '../constants/EventConstants';
+import { CATEGORY_OF_MENUS, PRICE_OF_MENUS } from '../constants/MenuInfo';
 import controllerUtills from '../utills/controllerUtills';
 import InputView from '../view/InputView';
 import OutputView from '../view/OutputView';
@@ -10,46 +20,18 @@ class EventController {
   }
 
   async startEvent() {
-    await this.manufactureDataOfModel();
+    await this.setAllDatasOfModel();
 
     this.printResultOfEvent();
   }
 
-  async manufactureDataOfModel() {
+  async setAllDatasOfModel() {
     this.#model.date = await InputView.readDate();
     this.#model.menus = await InputView.readOrder();
     this.setModelAmount();
     this.setModelBenefitAmountList();
     this.setModelBenefitAmount();
     this.setModelDiscountAmount();
-  }
-
-  setModelAmount() {
-    this.#model.amount = controllerUtills.calculateTotalAmountBeforeDiscount(
-      this.#model.menus
-    );
-  }
-
-  setModelBenefitAmountList() {
-    this.#model.benefitAmountList = controllerUtills.calculateBenefitAmountList(
-      this.#model.amount,
-      this.#model.date,
-      this.#model.menus
-    );
-  }
-
-  setModelBenefitAmount() {
-    this.#model.benefitAmount = controllerUtills.calculateBenefitAmount(
-      this.#model.amount,
-      this.#model.benefitAmountList
-    );
-  }
-
-  setModelDiscountAmount() {
-    this.#model.discountAmount = controllerUtills.calculateTotalDiscountAmount(
-      this.#model.amount,
-      this.#model.benefitAmount
-    );
   }
 
   printResultOfEvent() {
@@ -66,6 +48,114 @@ class EventController {
       this.#model.amount
     );
     OutputView.printDecemberEventBadge(this.#model.discountAmount);
+  }
+
+  setModelAmount() {
+    this.#model.amount = this.calculateTotalAmountBeforeDiscount(
+      this.#model.menus
+    );
+  }
+
+  setModelBenefitAmountList() {
+    this.#model.benefitAmountList = this.calculateBenefitAmountList();
+  }
+
+  setModelBenefitAmount() {
+    this.#model.benefitAmount = controllerUtills.calculateBenefitAmount(
+      this.#model.amount,
+      this.#model.benefitAmountList
+    );
+  }
+
+  setModelDiscountAmount() {
+    this.#model.discountAmount = controllerUtills.calculateTotalDiscountAmount(
+      this.#model.amount,
+      this.#model.benefitAmount
+    );
+  }
+
+  calculateTotalAmountBeforeDiscount() {
+    let amount = 0;
+    this.#model.menus.forEach((menu) => {
+      amount += Number(PRICE_OF_MENUS[menu.name]) * Number(menu.quantity);
+    });
+
+    return amount;
+  }
+
+  calculateBenefitAmountList() {
+    const benefitAmountList = [...BENEFIT_LIST];
+    benefitAmountList[0].push(this.checkChirsmasDdayEvent());
+    benefitAmountList[1].push(this.checkWeekdayDiscountEvent());
+    benefitAmountList[2].push(this.checkWeekendDiscountEvent());
+    benefitAmountList[3].push(this.checkSpecialDiscountEvent());
+    benefitAmountList[4].push(this.checkGiveawayEvent());
+    return benefitAmountList;
+  }
+
+  checkGiveawayEvent() {
+    if (this.#model.amount >= GIVEAWAY_EVENT.APPLICABLE_AMOUNT) {
+      return PRICE_OF_MENUS.샴페인;
+    }
+    return STATUS.NON_APPLICABLE_AMOUNT;
+  }
+
+  checkChirsmasDdayEvent() {
+    if (this.#model.date > CHRISMAS_D_DAY.EVENT_DEADLINE) {
+      return STATUS.NON_APPLICABLE_AMOUNT;
+    }
+
+    return (
+      CHRISMAS_D_DAY.DEFAULT_DISCOUNT +
+      CHRISMAS_D_DAY.BONUS_DISCOUNT * (this.#model.date - 1)
+    );
+  }
+
+  checkWeekdayDiscountEvent() {
+    if (WEEKDAY_EVENT.NON_APPLICABLE_DATES.includes(this.#model.date)) {
+      return STATUS.NON_APPLICABLE_AMOUNT;
+    }
+
+    const discountAmount = this.calculateDayEventDiscountAmount(
+      this.#model.menus,
+      [...CATEGORY_OF_MENUS.DESSERT]
+    );
+
+    return discountAmount;
+  }
+
+  checkWeekendDiscountEvent() {
+    if (!WEEKDAY_EVENT.NON_APPLICABLE_DATES.includes(this.#model.date)) {
+      return STATUS.NON_APPLICABLE_AMOUNT;
+    }
+
+    const discountAmount = this.calculateDayEventDiscountAmount(
+      this.#model.menus,
+      [...CATEGORY_OF_MENUS.MAIN]
+    );
+
+    return discountAmount;
+  }
+
+  checkSpecialDiscountEvent() {
+    if (!SPECIAL_EVENT.APPLICABLE_DATES.includes(this.#model.date)) {
+      return STATUS.NON_APPLICABLE_AMOUNT;
+    }
+    const discountAmount = 1000;
+    return discountAmount;
+  }
+
+  calculateDayEventDiscountAmount(category) {
+    const discountAmount = this.#model.menus.reduce((tempAmount, menu) => {
+      let amount = tempAmount;
+      if (category.includes(menu.name)) {
+        amount += EVENT.DISCOUNT * menu.quantity;
+        return amount;
+      }
+      return amount;
+    }, 0);
+
+    return discountAmount;
   }
 }
 
